91371216da21d42a4dc8b3a94b339a06d63eb055
dstar1 <harrisonfsmith95@gmail.com>
2019-05-03 18:06:55 -0700

    connecting mongoDB to application

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+
 
 const app = express();
+
+// DB Config
+const db = require("./config/keys").MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Mongo DB Conected..."))
+    .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
