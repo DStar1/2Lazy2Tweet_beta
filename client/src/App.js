2f870bdf3129db897b19f51391fc77143e6af43e
dstar1 <harrisonfsmith95@gmail.com>
2019-05-14 21:09:45 -0700

    post working client side and mongoDB perssisting data. CSS looking much better. Added date and time picker. Now working on authenticating session token.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './App.css';
 import FormEnter from './Form.js';
 import axios from 'axios';
+import Auth from './modules/auth';
+// import { connect } from 'react-redux';
 
-export default class PostList extends React.Component {
+// const NoMatch = () => (
+//   <p>Path not found</p>
+// );
+
+class PostList extends React.Component {
   state = {
     posts: []
   }
@@ -17,23 +23,54 @@ export default class PostList extends React.Component {
   }
 
   render() {
+    // const { isAuthenticated } = this.props.auth;
+
+    // const userLinks = (
+    //   <ul className="nav navbar-nav navbar-right">
+    //     <li><a href="#" onClick={this.logout.bind(this)}>Logout</a></li>
+    //   </ul>
+    // );
+
+    // const guestLinks = (
+    //   <ul className="nav navbar-nav navbar-right">
+    //     <li><a href="#" onClick={this.logout.bind(this)}>Logout</a></li>
+    //   </ul>
+    // );
+    
+    //////// FUCK SESSIONS /////////
+    if (Auth.isUserAuthenticated()) return("Login");
+    else {
     return (
-      <div class="elem" id="elem">
-        <h1 class="App-header" id="App-header">2Lazy2Tweet</h1>
+      <div className="elem" id="elem">
+        <h1 className="App-header" id="App-header">2Lazy2Tweet</h1>
         <FormEnter />
         { this.state.posts.map(post =>
-          <div class="posts" id="posts">
-            <h class="dt" id="dt">Date:</h>
-            <br></br>
-            <h class="dt" id="dt">{post.date}</h>
-            <br></br>
-            <h class="post" id="post">Post:</h>
-            <br></br>
-            <p class="post" id="post">{post.post}</p>
-          </div>
+            <div>
+              <div className="posts" id="posts">
+                <h className="dt" id="dt">Date:</h>
+                <br></br>
+                <h className="dt" id="dt">{post.dateToPost}</h>
+                <br></br>
+                <h className="post" id="post">Post:</h>
+                <br></br>
+                <p className="post" id="post">{post.post}</p>
+              </div>
+              <div className="break" id="break"></div>
+            </div>
       )}
+      <a href="http://localhost:8080/users/logout" classNmae="btn btn-secondary">Logout</a>
+      <br></br>
+      {document.getElementById("sid")}
       </div>
     )
   }
 }
+}
+// }
+// function mapStateToProps(state) {
+//   return {
+//     auth: state.auth
+//   };
+// }
 
+export default PostList;//connect(mapStateToProps)(PostList);
--- client/src/Form.js
+++ client/src/Form.js
@@ -1,34 +1,102 @@
 import React from 'react';
+import axios from 'axios';
 import './Form.css'
+import { Calendar, CalendarCell, CalendarWeekCell, CalendarNavigationItem, CalendarHeaderTitle, DateInput, DatePicker, TimePicker, MultiViewCalendar, DateRangePicker, DateTimePicker } from '@progress/kendo-react-dateinputs'
+import '@progress/kendo-react-intl'
+import '@progress/kendo-react-tooltip'
+import '@progress/kendo-react-common'
+import '@progress/kendo-react-popup'
+import '@progress/kendo-date-math'
+
 
 export default class FormEnter extends React.Component {
-    handleSubmit(event) {
-        console.log("YOYOYOOYO");
-      }
-    render() {
-      return (
-        <div >
-        <form onSubmit={this.handleSubmit}>
-        <ul class="form-style-1">
-            <li><label>Full Name <span class="required">*</span></label><input type="text" name="field1" class="field-divided" placeholder="First" /> <input type="text" name="field2" class="field-divided" placeholder="Last" /></li>
-            <li>
-                <label>Subject</label>
-                <select name="field4" class="field-select">
-                <option value="Advertise">Advertise</option>
-                <option value="Partnership">Partnership</option>
-                <option value="General Question">General</option>
-                </select>
-            </li>
-            <li>
-                <label>Your Message <span class="required">*</span></label>
-                <textarea name="field5" id="field5" class="field-long field-textarea"></textarea>
-            </li>
-            <li>
-                <input type="submit" value="Submit" />
-            </li>
-        </ul>
-        </form>
-        </div>
-      )
+  constructor(props){
+    super(props);
+    this.state={
+      'dateToPost': '',
+      'post': ''
     }
   }
+  handleDate(e){
+    // console.log(e.target.value);
+    this.setState({
+      dateToPost: e.target.value
+    });
+  }
+  handlePost(e){
+    // console.log(e.target.value);
+    this.setState({
+      post: e.target.value
+    });
+  }
+  handleSubmit(e){
+     //call the api here with current state value (this.state.username)
+    //  console.log(this.state);
+     axios.post("/api/posts", this.state, {
+      headers: {
+          'accept': 'application/json',
+          'accept-language': 'en_US'
+      }
+    });
+  }
+  render(){
+   return(
+     <div>
+      <form>
+      <DateTimePicker 
+                            // name="date"
+                            // value={this.state.post}
+                            // onChange={e => this.setState({ post: e.target.value })}
+                            onChange={this.handleDate.bind(this)}
+                />
+        {/* <input onChange={this.handleDate.bind(this)} type="text" name="dateToPost" placeholder="Enter date here" /> */}
+        <input onChange={this.handlePost.bind(this)} type="text" name="post" placeholder="Enter post here" />
+        <button onClick={this.handleSubmit.bind(this)}>Submit</button>
+      </form>
+      <p>{JSON.stringify(this.state)}</p>
+    </div>
+   )
+    }
+}
+ 
+
+
+
+
+
+// export default class FormEnter extends React.Component {
+//     handleSubmit(event) {
+//         console.log("YOYOYOOYO");
+//         axios.post("/api/posts")
+//         .then(res => {
+//           const posts = res.data;
+//           this.setState({ posts });
+//         })
+//       }
+//     render() {
+//       return (
+//         <div >
+//         <form onSubmit={this.handleSubmit}>
+//         <ul className="form-style-1">
+//             <li><label>Full Name <span className="required">*</span></label><input type="text" name="field1" className="field-divided" placeholder="First" /> <input type="text" name="field2" className="field-divided" placeholder="Last" /></li>
+//             <li>
+//                 <label>Subject</label>
+//                 <select name="field4" className="field-select">
+//                 <option value="Advertise">Advertise</option>
+//                 <option value="Partnership">Partnership</option>
+//                 <option value="General Question">General</option>
+//                 </select>
+//             </li>
+//             <li>
+//                 <label>Your Message <span className="required">*</span></label>
+//                 <textarea name="field5" id="field5" className="field-long field-textarea"></textarea>
+//             </li>
+//             <li>
+//                 <input type="submit" value="Submit" />
+//             </li>
+//         </ul>
+//         </form>
+//         </div>
+//       )
+//     }
+//   }
